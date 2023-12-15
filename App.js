61fd2dfdc2ec68603a97995f18495a33cbc6bde1
Vannam Manasa <manasavannam2478@gmail.com>
2023-12-15 15:37:54 +0530

    episode-03

--- App.js
+++ App.js
@@ -4,12 +4,23 @@ import ReactDOM from "react-dom/client";
 
 
 
-const parent = React.createElement("div",{},"Manasa Royal");
 
+const number = 1000;
+//const parent = React.createElement("div",{},"Manasa Royal");
+const Title = () =>(
+  <h1 id="heading" className="heading">Namaste React using JSX</h1>
+);
 
- 
+const HeadingComponent = () =>(
+  <div id ="container">
+        <Title />
+        <h2>{number} </h2>
+        <h1>Learning React</h1>
 
+  </div>
+
+);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
 
