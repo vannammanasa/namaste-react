99975fa5a1dfd9d5fc7bb2fa638cfcd7971a65c7
Manasa Vannam <66632506+vannammanasa@users.noreply.github.com>
2023-12-15 15:50:22 +0530

    Added episode-03 "Laying the foundation"

---
+++ Episode-03/App.js
@@ -0,0 +1,26 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+
+const number = 1000;
+//const parent = React.createElement("div",{},"Manasa Royal");
+const Title = () =>(
+  <h1 id="heading" className="heading">Namaste React using JSX</h1>
+);
+
+const HeadingComponent = () =>(
+  <div id ="container">
+        <Title />
+        <h2>{number} </h2>
+        <h1>Learning React</h1>
+
+  </div>
+
+);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
+
