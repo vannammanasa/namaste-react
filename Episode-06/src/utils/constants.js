78d296f0700e2973db9827169d8b87219fe5c0de
Vannam Manasa <manasavannam2478@gmail.com>
2024-01-17 11:49:34 +0530

    Episode-06 - Exploring the World

---
+++ Episode-06/src/utils/constants.js
@@ -0,0 +1,4 @@
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL = "https://tse3.mm.bing.net/th?id=OIP.DJXkSF9B--LYE8SM7M8yCAHaHa&pid=Api&P=0&h=220/";
+
